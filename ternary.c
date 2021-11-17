b9b8a47c057a2cd8a34abf7137172850a237695a
shivam6351 <75487392+shivam6351@users.noreply.github.com>
2021-11-17 10:53:02 +0530

    spell check

--- ternary.c
+++ ternary.c
@@ -77,7 +77,49 @@ int search(struct node* root,char *word)
 	}
 }
 
-
+void suggest(struct node* root,char *word,char *real,int c)
+{
+	if(root==NULL)
+	{
+		return ;
+	}
+	if(*word<(root)->ch)
+	{
+		if(root->left!=NULL &&   root->left->ch==word[0])
+		{
+			c=1;
+		}
+		
+		if(root->left!=NULL && c==1){
+			
+        strncat(real,&root->left->ch,1);
+		}
+		return suggest(root->left,word,real,c);
+	}
+	else if(*word>(root)->ch)
+	{
+		if(root->right!=NULL &&   root->right->ch==word[0])
+		{
+			c=1;
+		}
+		if(root->right!=NULL && c==1){
+		strncat(real,&root->right->ch,1);
+		}
+		return suggest(root->right,word,real,c);
+	}
+	else
+	{
+		   if(root->equal!=NULL && root->equal->ch==word[0])
+		{
+			c=1;
+		}
+		    if(root->equal!=NULL && c==1){
+		    strncat(real,&root->equal->ch,1);
+			}
+			return suggest(root->equal,word+1,real,c);
+		
+	}
+}
 
 int main()
 {
@@ -89,6 +131,7 @@ int main()
 	}
     struct node* root=NULL;
 	char a;
+	char real[100]="";
 	char word[100]="";
 	
 	          while ((a=fgetc(fptr))!=EOF)
@@ -110,8 +153,18 @@ int main()
 	{
 		if(c==' ' || c=='\n')
 		{
-			search(root,s)==1 ? printf("%s ",s):printf("\e[4m%s\e[0m ",s);
-			printf("\e[0m");
+			
+			if(search(root,s))
+			{
+				printf("%s ",s);
+			}
+			else{
+				int c=0;
+				suggest(root,s,real,c);
+				printf(" \e[4m%s\e[0m (did you mean %s)",s,real);
+				memset(real,0,strlen(real));
+			}
+			
 			memset(s,0,strlen(s));
 		}
 		else
