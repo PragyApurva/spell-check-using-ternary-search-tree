e2bece1e226851005b0761f8a3866500d9596dcb
HardKofi <2020mcb1229@iitrpr.ac.in>
2021-11-21 18:58:36 +0530

    Added comments

--- ternary.c
+++ ternary.c
@@ -1,12 +1,15 @@
+// Spell checking program in c using ternert search tree
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-struct node
+//Node of ternary search tree
+struct node 
 {
-	char ch;
-	int end;
+	char ch; //character to enter
+	int end; //determine if word is ended here or not
+	//3 children of tst
 	struct node *left;
 	struct node *right;
 	struct node *equal;
@@ -25,8 +28,10 @@ struct qnode
 	struct qnode *next;
 };
 
+// Function to create a new tst node
 struct node *newnode(char ch)
 {
+	//initializing values
 	struct node *newnode = (struct node *)malloc(sizeof(struct node));
 	newnode->ch = ch;
 	newnode->end = 0;
@@ -34,25 +39,31 @@ struct node *newnode(char ch)
 	return newnode;
 }
 
+//This function will insert word in tst
 void insert(struct node **root, char *word)
 {
+	//If tree is empty create a new node
 	if (*root == NULL)
 	{
 		*root = newnode(*word);
 	}
+
+	//If current word character is smaller than root's character
 	if ((*word) < (*root)->ch)
 	{
-		insert(&((*root)->left), word);
+		insert(&((*root)->left), word); // inserting word in left sub tree
 	}
+	//If current word character is grester than root's character
 	else if ((*word) > (*root)->ch)
 	{
-		insert(&((*root)->right), word);
+		insert(&((*root)->right), word); // inserting word in Right sub tree
 	}
+	//If current word character is same as that of root's character
 	else
 	{
 		if (*(word + 1))
 		{
-			insert(&((*root)->equal), word + 1);
+			insert(&((*root)->equal), word + 1); // inserting word in equal sub tree
 		}
 		else
 		{
@@ -61,35 +72,36 @@ void insert(struct node **root, char *word)
 	}
 }
 
+// Function to search word in tst
 int search(struct node *root, char *word)
 {
-	if (root == NULL)
+	if (root == NULL) // if tree is empty
 	{
 		return 0;
 	}
-	if (*word < (root)->ch)
+	//If current word's character is smaller than root's character
+	if (*word < (root)->ch) 
 	{
-
-		return search(root->left, word);
+		return search(root->left, word); // search the word in left sub tree
 	}
+	//If current word's character is smaller than root's character
 	else if (*word > (root)->ch)
 	{
-		return search(root->right, word);
+		return search(root->right, word); // search the word in right sub tree
 	}
 	else
 	{
-		if (*(word + 1) == '\0')
+		if (*(word + 1) == '\0') // If word ends return end
 		{
-
 			return root->end;
 		}
-		else
+		else // if not end then call function recursively
 		{
-
 			return search(root->equal, word + 1);
 		}
 	}
 }
+
 
 void final(struct node *root, char *str, char *ret, struct node *temp)
 {
@@ -97,7 +109,6 @@ void final(struct node *root, char *str, char *ret, struct node *temp)
 
 	while (search(temp, ret) != 1)
 	{
-
 		strncat(ret, &(root->ch), 1);
 		if (search(temp, ret))
 		{
@@ -108,32 +119,35 @@ void final(struct node *root, char *str, char *ret, struct node *temp)
 	}
 }
 
+//suggest a word corrosponding to inputed word's preffix 
+//If corrosponding word prefix not in our dictonary then doesn't return anything
 void suggest(struct node *root, char *str, char *ret, struct node *temp)
 {
-	char **substr = malloc(100 * sizeof(char *));
-	for (int i = 0; i < 100; i++)
+	char **substr = malloc(100 * sizeof(char *)); 
+	for (int i = 0; i < 100; i++) // 100 is max charecter size of word
 	{
 		substr[i] = malloc(100 * sizeof(char));
 		substr[i] = "";
 	}
 	struct node *prev = NULL;
-	int n = strlen(str);
+	int n = strlen(str); // check word size of word
 	int i = 0;
+	// Traversing whole word character by charecter
 	while (i < n)
 	{
-		if (root == NULL)
+		if (root == NULL) // if tree is empty return
 			return;
-		if (root->ch == str[i])
+		if (root->ch == str[i]) //if charecter matches with root's child
 		{
 			prev = root;
-			root = root->equal;
+			root = root->equal; // make equal our new root
 			i++;
 			continue;
 		}
-		if (root->ch < str[i])
-			root = root->right;
-		else
-			root = root->left;
+		if (root->ch < str[i]) //if current charecter is greater then charecter corresponding to current word
+			root = root->right; // make right sub tree of root to new root
+		else //if current charecter is less then charecter corresponding to current word
+			root = root->left; // make left sub tree of root to new root
 	}
 	strncpy(ret, str, strlen(str));
 	final(root, str, ret, temp);
@@ -141,68 +155,73 @@ void suggest(struct node *root, char *str, char *ret, struct node *temp)
 
 int main()
 {
-	FILE *fptr = fopen("./words.txt", "r");
+	FILE *fptr = fopen("./words.txt", "r"); // reading dictonary file
 	if (fptr == NULL)
 	{
-		printf("Error openeing the file");
+		printf("Error openeing the file"); // print Error if not able to open the file
 		exit(1);
 	}
-	struct node *root = NULL;
-	char a;
-	char real[100] = "";
-	char word[100] = "";
+	
+	struct node *root = NULL; // root node
+	char a; // character to take word from user character by character
+	char real[100] = ""; 
+	char word[100] = ""; // user input
 
-	while ((a = fgetc(fptr)) != EOF)
+	// Insert wordlist file in tst
+	while ((a = fgetc(fptr)) != EOF) 
 	{
-		if (a == '\n')
+		if (a == '\n') // if word ends insert it
 		{
 			insert(&root, word);
-			memset(word, 0, strlen(word));
+			memset(word, 0, strlen(word)); // empty word after insertion in tst 
 		}
 		else
 		{
-			strncat(word, &a, 1);
+			strncat(word, &a, 1); // Appends 1 character from a to word
 		}
 	}
 
 	char s[100] = "";
-	char c;
+	char c; // user input
 	char cha;
 	struct node *temp = root;
-	while (c = getchar())
+	while (c = getchar()) // Taking input from user
 	{
 		char **substr;
-		c = tolower(c);
+		c = tolower(c); 
+
+		// if word terminates search word tst
 		if (c == ' ' || c == '\n' || c == ',' || c == '.' || c == '!' || c == '?' || c == ';' || c == ':')
 		{
-
+			//if found return that word
 			if (search(root, s))
 			{
 				printf("%s ", s);
 			}
+			// If not found print word with an underline
 			else
 			{
-				suggest(root, s, real, temp);
+				suggest(root, s, real, temp); // Suggest word accoding to word prefix
 				root = temp;
-				printf(" \e[4m%s\e[0m ", s);
-				if (strlen(real) != 0)
+				printf(" \e[4m%s\e[0m ", s); // print word with underline
+				// if we find a word to suggest print suggested word is not then no suggestion
+				if (strlen(real) != 0) 
 				{
 					printf("(did you mean: ");
 					printf("%s", real);
-
 					printf(")");
 				}
-				memset(real, 0, strlen(real));
+				memset(real, 0, strlen(real)); // empty real variable
 			}
-
-			memset(s, 0, strlen(s));
+			memset(s, 0, strlen(s)); // empty s
 		}
 		else
 		{
-			strncat(s, &c, 1);
+			strncat(s, &c, 1); // append 1 character from c pointer to s
 		}
-		int val = strcmp(s, "exit");
+		int val = strcmp(s, "exit"); // if we type exit then exit the program
 		if (val == 0)
 			return 0;
 	}
+	printf("\n");
 }
